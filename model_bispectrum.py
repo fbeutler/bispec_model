8a2728f7d29e96d80c2e5aae633f58aa8ce03350
Florian Beutler <florian.beutler@port.ac.uk>
2019-02-18 11:45:49 +0000

    Update model_bispectrum.py

--- model_bispectrum.py
+++ model_bispectrum.py
@@ -20,7 +20,7 @@ import nbodykit
 from helper_functions import read_files
 from helper_functions import para_global_GR_sims as para
 
-ep = 0.02
+ep = 0.0001
 grid = 512
 
 
@@ -48,9 +48,9 @@ def fg(z=para.redshift):
 	return para.cosmo.scale_independent_growth_rate(z)
 	#return para.fgrowth
 
-def Om(z=para.redshift):
-	''' Return \Omega_m(z) '''
-	return para.cosmo.Omega_m(z)
+def Ocdm(z=para.redshift):
+	''' Return \Omega_cdm(z) '''
+	return para.cosmo.Omega_cdm(z)
 
 def E(z=para.redshift): 
 	''' Return the dimensionless Hubble parameter H(z)/H0 '''
@@ -82,7 +82,7 @@ def B10(z=para.redshift):
 
 def B20(z=para.redshift):
 	''' Second order bias parameter b2 '''
-	return para.b2
+	return para.b2[0]
 
 def Tidalbias(z=para.redshift):
 	''' 
@@ -101,7 +101,7 @@ def A(z=para.redshift):
 	- aHz_in_invdist has unit (Mpc/h)^-1
 	- chi has unit (Mpc/h)
 	'''
-	return bevo(z) + 1.5*Om(z) - 3. + (2. - 5.*LumS(z))*(1. - 1./(chi(z)*aHz_in_invdist(z)))
+	return bevo(z) + 1.5*Ocdm(z) - 3. + (2. - 5.*LumS(z))*(1. - 1./(chi(z)*aHz_in_invdist(z)))
 
 def C(z=para.redshift):
 	''' 
@@ -114,7 +114,7 @@ def C(z=para.redshift):
 
 def EA(z=para.redshift):
 	''' see eq. 4.14 '''
-	return 4. - 2.*A(z) - 1.5*Om(z)
+	return 4. - 2.*A(z) - 1.5*Ocdm(z)
 
 def F2(k1, k2, k3):
 	''' 
@@ -123,7 +123,8 @@ def F2(k1, k2, k3):
 	'''
 	k1dk2 = (k3**2 - k1**2 - k2**2)/2.
 	mu = k1dk2/(k1*k2)
-	return 5./7. + 0.5*k1dk2*(1./(k1**2) + 1./(k2**2)) + 2.*mu**2/7.
+	F2 = 5./7. + 0.5*k1dk2*(1./(k1**2) + 1./(k2**2)) + 2.*mu**2/7.
+	return 2.*F2
 
 def G2(k1, k2, k3):
 	''' 
@@ -132,16 +133,17 @@ def G2(k1, k2, k3):
 	'''
 	k1dk2 = (k3**2 - k1**2 - k2**2)/2.
 	mu = k1dk2/(k1*k2)
-	return 3./7. + 0.5*k1dk2*(1./k1**2 + 1./k2**2) + 4.*mu**2/7.
+	G2 = 3./7. + 0.5*k1dk2*(1./k1**2 + 1./k2**2) + 4.*mu**2/7.
+	return 2.*G2
 
 def S2(k1, k2, k3):
 	'''
 	S2 kernel
 	(see eq. 4.8)
 	'''
-	k1dk2 = (k3**2 - k1**2 - k2**2)/2.;
-	mu = k1dk2/(k1*k2);
-	return mu**2 - 1./3.;
+	k1dk2 = (k3**2 - k1**2 - k2**2)/2.
+	mu = k1dk2/(k1*k2)
+	return mu**2 - 1./3.
 
 def Z1(z, mu):    
 	'''
@@ -174,8 +176,8 @@ def KD2(z, k1, k2, k3, mu1, mu2, mu3):
 	(see eq. 4.12)
 	'''
 	mu12 = -(k1**2. + k2**2. -(k3**2.))/(2.*k1*k2)
-	line1 = -1.5*(mu1*k1/k2**2 + mu2*k2/k1**2)*Om(z)*B10(z) + 2.*mu12*(mu1/k2 + mu2/k1)*fg(z)**2 + (mu1/k1 + mu2/k2)*C(z)*fg(z)
-	line2 = -1.5*(mu1**3*k1/k2**2 + mu2**3*k2/k1**2)*Om(z)*fg(z) + mu1*mu2*(mu1/k2 + mu2/k1)*(1.5*Om(z) - EA(z)*fg(z))*fg(z)
+	line1 = -1.5*(mu1*k1/k2**2 + mu2*k2/k1**2)*Ocdm(z)*B10(z) + 2.*mu12*(mu1/k2 + mu2/k1)*fg(z)**2 + (mu1/k1 + mu2/k2)*C(z)*fg(z)
+	line2 = -1.5*(mu1**3*k1/k2**2 + mu2**3*k2/k1**2)*Ocdm(z)*fg(z) + mu1*mu2*(mu1/k2 + mu2/k1)*(1.5*Ocdm(z) - EA(z)*fg(z))*fg(z)
 	line3 = (mu3/k3)*G2(k1,k2,k3)*A(z)*fg(z)
 	return aHz_in_invdist(z)*(line1 + line2 + line3)
 
@@ -195,61 +197,82 @@ def DipoleBispec(z, k1, k2, k3, mu1, mu2, mu3):
 	tmp2 = (Z1(z, mu1)*KD1(z, k2, mu2) + KD1(z, k1, mu1)*Z1(z, mu2))*Z2(z, k1, k2, k3, mu1, mu2, mu3)
 	return tmp1 + tmp2
 
-def B_sq(z, k1, k2, Theta_12, Omega, Phi): 
-	''' Calculate the bispectrum for a given k1 and k2 '''
-	# K3=np.sqrt((K2*np.sin(Theta_12))**2. + (K1 + K2*np.cos(Theta_12))**2. )
-	k3 = np.sqrt(k1**2 + k2**2 + 2.*k1*k2*np.cos(Theta_12))
-	mu_1 = np.cos(Omega)
-	mu_2 = np.sin(Theta_12)*np.sin(Omega)*np.cos(Phi) + np.cos(Theta_12)*np.cos(Omega)
-	mu_3 = (-k1/k3)*mu_1 + (-k2/k3)*mu_2
+# def B_sq(z, k1, k2, Theta_12, Omega, Phi): 
+# 	''' Calculate the bispectrum for a given k1 and k2 '''
+# 	# K3=np.sqrt((K2*np.sin(Theta_12))**2. + (K1 + K2*np.cos(Theta_12))**2. )
+# 	k3 = np.sqrt(k1**2 + k2**2 + 2.*k1*k2*np.cos(Theta_12))
+# 	mu_1 = np.cos(Omega)
+# 	mu_2 = np.sin(Theta_12)*np.sin(Omega)*np.cos(Phi) + np.cos(Theta_12)*np.cos(Omega)
+# 	mu_3 = (-k1/k3)*mu_1 + (-k2/k3)*mu_2
 	  
-	BgN = np.zeros((len(Theta_12)))
-	BgD = np.zeros((len(Theta_12)))
-	for i in range(len(Theta_12)): 
-	    BgN[i] = (NewtonainBispec(z, k1, k2, k3[i], mu_1, mu_2[i], mu_3[i])*(para.Pk_class(k1)*para.Pk_class(k2))
-	            + NewtonainBispec(z, k2, k3[i], k1, mu_2[i], mu_3[i], mu_1)*para.Pk_class(k2)*para.Pk_class(k3[i])
-	            + NewtonainBispec(z, k3[i], k1, k2, mu_3[i], mu_1, mu_2[i])*(para.Pk_class(k3[i])*para.Pk_class(k1)))
+# 	BgN = np.zeros((len(Theta_12)))
+# 	BgD = np.zeros((len(Theta_12)))
+# 	for i in range(len(Theta_12)): 
+# 	    BgN[i] = (NewtonainBispec(z, k1, k2, k3[i], mu_1, mu_2[i], mu_3[i])*(para.Pk_class(k1)*para.Pk_class(k2))
+# 	            + NewtonainBispec(z, k2, k3[i], k1, mu_2[i], mu_3[i], mu_1)*para.Pk_class(k2)*para.Pk_class(k3[i])
+# 	            + NewtonainBispec(z, k3[i], k1, k2, mu_3[i], mu_1, mu_2[i])*(para.Pk_class(k3[i])*para.Pk_class(k1)))
 	 
-	    BgD[i] = (DipoleBispec(z, k1, k2, k3[i], mu_1, mu_2[i], mu_3[i])*(para.Pk_class(k1)*para.Pk_class(k2))
-	            + DipoleBispec(z, k2, k3[i], k1, mu_2[i], mu_3[i], mu_1)*para.Pk_class(k2)*para.Pk_class(k3[i])
-	            + DipoleBispec(z, k3[i], k1, k2, mu_3[i], mu_1, mu_2[i])*(para.Pk_class(k3[i])*para.Pk_class(k1)))                                                       
-	return k3, BgN, BgD
+# 	    BgD[i] = (DipoleBispec(z, k1, k2, k3[i], mu_1, mu_2[i], mu_3[i])*(para.Pk_class(k1)*para.Pk_class(k2))
+# 	            + DipoleBispec(z, k2, k3[i], k1, mu_2[i], mu_3[i], mu_1)*para.Pk_class(k2)*para.Pk_class(k3[i])
+# 	            + DipoleBispec(z, k3[i], k1, k2, mu_3[i], mu_1, mu_2[i])*(para.Pk_class(k3[i])*para.Pk_class(k1)))                                                       
+# 	return k3, BgN, BgD
 
-def BNgggintegrand(Omega, Phi, z, k1, k2, k3, P1, P2, P3):
+def BNgggintegrand(Omega, Phi, z, k1, k2, k3, P1, P2, P3, L, M):
 	''' Integrant of eq. 5.13 in the Newtonian limit given k1, k2, k3, L=0 and M=0 '''
+	#print("BNggg Omega = ", Omega)
+	#print("BNggg Phi = ", Phi)
 	costheta12 = -(k1**2. + k2**2. -(k3**2.))/(2.*k1*k2)
+	#theta12 = np.arccos(costheta12)
 	mu_1 = np.cos(Omega)
-	mu_2 = mu_1*costheta12 - math.sqrt(1. - mu_1**2)*np.cos(Phi)*math.sqrt(abs(1. - costheta12**2))
+	#mu_2 = mu_1*costheta12 - math.sqrt(1. - mu_1**2)*np.cos(Phi)*math.sqrt(abs(1. - costheta12**2))
+	#mu_2 = mu_1*costheta12 + np.sqrt(1. - mu_1**2)*np.sin(theta12)*np.sin(Phi)
+	mu_2 = mu_1*costheta12 - np.sqrt(1. - mu_1**2)*np.sqrt(1. - costheta12**2)*np.sin(Phi)
 	mu_3 = (-k1/k3)*mu_1 + (-k2/k3)*mu_2
+	#print("BNggg mu_1 = ", mu_1)
+	#print("BNggg mu_2 = ", mu_2)
+	#print("BNggg mu_3 = ", mu_3)
 	    
 	BgN = (NewtonainBispec(z, k1, k2, k3, mu_1, mu_2, mu_3)*P1*P2 
 		 + NewtonainBispec(z, k2, k3, k1, mu_2, mu_3, mu_1)*P2*P3
 	     + NewtonainBispec(z, k3, k1, k2, mu_3, mu_1, mu_2)*P3*P1)
-	return np.real(BgN*sp.sph_harm(0, 0, Omega, Phi))/math.sqrt(4.*np.pi)
+	# The first argument of sph_harm goes from [0, 2\pi] the second goes from [0, \pi]
+	return np.real(BgN*sp.sph_harm(M, L, Phi, Omega))/np.sqrt(4.*np.pi*(2.*L + 1.))
 
 def BDgggintegrand(Omega, Phi, z, k1, k2, k3, P1, P2, P3, L, M):
 	''' Integrant of eq. 5.13 in the Newtonian limit given k1, k2, k3 '''
-	costheta12 = -(k1**2. + k2**2. -(k3**2.))/(2.*k1*k2)
+	#print("BDggg Omega = ", Omega)
+	#print("BDggg Phi = ", Phi)
+	costheta12 = -(k1**2 + k2**2 -(k3**2))/(2.*k1*k2)
+	#theta12 = np.arccos(costheta12)
 	mu_1 = np.cos(Omega)
-	mu_2 = mu_1*costheta12 - math.sqrt(1. - mu_1**2)*np.cos(Phi)*math.sqrt(abs(1. - costheta12**2))
-	mu_3 = (-k1/k3)*mu_1 + (-k2/k3)*mu_2
+	#mu_2 = mu_1*costheta12 - math.sqrt(1. - mu_1**2)*np.cos(Phi)*math.sqrt(abs(1. - costheta12**2))
+	#mu_2 = mu_1*costheta12 + np.sqrt(1. - mu_1**2)*np.sin(theta12)*np.sin(Phi)
+	mu_2 = mu_1*costheta12 - np.sqrt(1. - mu_1**2)*np.sqrt(1. - costheta12**2)*np.sin(Phi)
+	mu_3 = -(k1/k3)*mu_1 - (k2/k3)*mu_2
+	#print("BDggg mu_1 = ", mu_1)
+	#print("BDggg mu_2 = ", mu_2)
+	#print("BDggg mu_3 = ", mu_3)
 	    
-	BgD = (DipoleBispec(z, k1, k2, k3, mu_1, mu_2, mu_3)*P1*P2 
+	BgD = (DipoleBispec(z, k1, k2, k3, mu_1, mu_2, mu_3)*P1*P2
 		 + DipoleBispec(z, k2, k3, k1, mu_2, mu_3, mu_1)*P2*P3
 	     + DipoleBispec(z, k3, k1, k2, mu_3, mu_1, mu_2)*P3*P1)
-	return np.real(BgD*sp.sph_harm(M, L, Phi, Omega))/math.sqrt(12.*np.pi)
+	if M == 0:
+		return np.real(BgD*sp.sph_harm(M, L, Phi, Omega))/np.sqrt(4.*np.pi*(2.*L + 1.))
+	else:
+		return np.imag(BgD*sp.sph_harm(M, L, Phi, Omega))/np.sqrt(4.*np.pi*(2.*L + 1.))
 
-def BNggg(z, k1, k2, k3, P1, P2, P3):
+def BNggg(z, k1, k2, k3, P1, P2, P3, L, M):
 	''' 
 	Calculate the bispectrum multipoles according to eq. 5.13 in the Newtonian limit
 	'''
 	time0 = time.time()
 	result = integrate.dblquad(BNgggintegrand,       # func
-							   0.002, 	             # \phi lower limit
-							   2.*np.pi-ep, 	     # \phi upper limit
-							   lambda Phi: 0.002,    # \Omega lower limit
+							   ep, 	             # \Phi lower limit
+							   2.*np.pi-ep, 	     # \Phi upper limit
+							   lambda Phi: ep,    # \Omega lower limit
 							   lambda Phi: np.pi-ep, # \Omega upper limit
-							   args=(z, k1, k2, k3, P1, P2, P3), epsabs=1.49e-8, epsrel=1.49e-8)
+							   args=(z, k1, k2, k3, P1, P2, P3, L, M), 
+							   epsabs=1.49e-8, epsrel=1.49e-8)
 	print("Calculated BNggg after %0.8f" % (time.time()-time0))
 	return result
 
@@ -259,7 +282,17 @@ def BDggg(z, k1, k2, k3, P1, P2, P3, L, M):
 	according to eq. 5.13
 	'''
 	time0 = time.time()
-	result = integrate.nquad(BDgggintegrand, [[0.002, 2.*np.pi-ep], [0.002, np.pi-ep]], args=(z, k1, k2, k3, P1, P2, P3, L, M), full_output=True)
+	# result = integrate.nquad(BDgggintegrand, 
+	# 						 [[0.002, 2.*np.pi-ep], [0.002, np.pi-ep]], 
+	# 						 args=(z, k1, k2, k3, P1, P2, P3, L, M), 
+	# 						 full_output=True)
+	result = integrate.dblquad(BDgggintegrand,       # func
+							   ep, 	             # \Phi lower limit
+							   2.*np.pi-ep, 	     # \Phi upper limit
+							   lambda Phi: ep,    # \Omega lower limit
+							   lambda Phi: np.pi-ep, # \Omega upper limit
+							   args=(z, k1, k2, k3, P1, P2, P3, L, M), 
+							   epsabs=1.49e-8, epsrel=1.49e-8)
 	print("Calculated BDggg after %0.8f" % (time.time()-time0))
 	return result
 
@@ -281,10 +314,12 @@ def Bg(z, k1, k2, Theta_12):
 	BgDm1 = np.zeros((len(Theta_12)))
 	for i in range(len(Theta_12)): 
 		print("Theta_12 = %d out of %d with theta_12 = %0.4f" % (i, len(Theta_12), Theta_12[i]))
-		BgN[i] = BNggg(z, k1, k2, k3[i], pk_k1, pk_k2, para.Pk_class(k3[i]))[0]
-		BgDm0[i] = BDggg(z, k1, k2, k3[i], pk_k1, pk_k2, para.Pk_class(k3[i]), 1, 0)[0]
-		BgDm1[i] = BDggg(z, k1, k2, k3[i], pk_k1, pk_k2, para.Pk_class(k3[i]), 1, 1)[0]
-	print("Finished Bmonopole_sq calculation after %0.8fsec" % (time.time()-time0))	                                                                              
+		pk_k3 = para.Pk_class(k3[i])
+		BgN[i] = BNggg(z, k1, k2, k3[i], pk_k1, pk_k2, pk_k3, 0, 0)[0]
+		BgDm0[i] = BDggg(z, k1, k2, k3[i], pk_k1, pk_k2, pk_k3, 1, 0)[0]
+		BgDm1[i] = BDggg(z, k1, k2, k3[i], pk_k1, pk_k2, pk_k3, 1, 1)[0]
+		print("BgDm0[i] = ", BgDm0[i])
+	print("Finished Bg calculation after %0.8fsec" % (time.time()-time0))	                                                                              
 	return Theta_12, BgN, BgDm0, BgDm1  
 
 def process_arrays(x):
@@ -311,7 +346,7 @@ def switch_basis(theta_12, BDm0, BDm1):
 	#'B101': [145988885.935935*sqrt(3), 235197172.791101*sqrt(3), 230931168.479705*sqrt(3), 113003189.532791*sqrt(3)]}
 	print("switch basis to (%d,%d,%d)" % (ell1,ell2,L))
 	BDm0_interp = interp1d(theta_12, BDm0)
-	BDm1_interp = interp1d(theta_12, BDm1) # B_{11} = B_{1-1}
+	BDm1_interp = interp1d(theta_12, BDm1) # B_{11} = -B_{1-1}
 	sp_factor = np.sqrt(4.*np.pi/(2.*ell2 + 1.))
 	int1 = lambda costh_12: wigner_3j(ell1,ell2,L,0,0,0)*sp_factor*sp.sph_harm(0, ell2, costh_12, 0.)*BDm0_interp(np.arccos(costh_12))   # M = 0
 	int2 = lambda costh_12: wigner_3j(ell1,ell2,L,0,-1,1)*sp_factor*sp.sph_harm(-1, ell2, costh_12, 0.)*BDm1_interp(np.arccos(costh_12)) # M = 1
@@ -322,11 +357,11 @@ def switch_basis(theta_12, BDm0, BDm1):
 	print("result = ", result)
 	if ell2 > 0:
 		###### is there a symmetry which makes the M=1 and M=-1 contributions to cancel out?
-		tmp = integrate.nquad(int1, [[-1+ep, 1-ep]], full_output=True)
+		tmp = integrate.nquad(int2, [[-1+ep, 1-ep]], full_output=True)
 		print("tmp = ", tmp)
 		result += tmp[0]
 		print("result = ", result)
-		tmp = integrate.nquad(int1, [[-1+ep, 1-ep]], full_output=True)
+		tmp = integrate.nquad(int3, [[-1+ep, 1-ep]], full_output=True)
 		print("tmp = ", tmp)
 		result += tmp[0]
 		print("result = ", result)
@@ -337,19 +372,21 @@ def switch_basis(theta_12, BDm0, BDm1):
 
 def plot_bi(theta_12, Bmono_N, BDm0, BDm1):
 	''' Plot the bispectrum in the Scoccimarro basis '''
-	plt.plot(theta_12, Bmono_N/para.cosmo.h**6, label=r'$l = 0$, Newtonian')
+	# Divide by h to get to Mpc^6
+	plt.plot(theta_12, Bmono_N/para.cosmo.h**6, color='k', label=r'$l = 0$, Newtonian')
+	plt.plot(theta_12, -1.*Bmono_N/para.cosmo.h**6, color='k', linestyle='--')
 	plt.plot(theta_12, BDm0/para.cosmo.h**6, color='red', label=r'$l = 1$, $m = 0$')
 	# Plot negative bispectrum as dashed line
 	plt.plot(theta_12, -1.*BDm0/para.cosmo.h**6, linestyle='--', color='red')
 	plt.plot(theta_12, BDm1/para.cosmo.h**6, color='blue', label=r'$l = 1$, $m = 1$')
 	# Plot negative bispectrum as dashed line
 	plt.plot(theta_12, -1.*BDm1/para.cosmo.h**6, linestyle='--', color='blue')
-	plt.plot(theta_12, abs(BDm0 + BDm1)/para.cosmo.h**6, label=r'$l = 1$, $m = \pm1$')
 	plt.yscale('log')
 	plt.legend(loc='best')
 	plt.title(r'Monopole and Dipole')
 	plt.xlabel(r'$\theta_{12}$')
 	plt.ylabel(r'$B({\bf k}_1, {\bf k}_2, {\bf k}_3)$ [Mpc$^6$]')
+	plt.savefig('%s/bi_model_theta12.png' % para.outpath, dpi=500)
 	plt.show()
 	return 
 
@@ -406,7 +443,7 @@ def main():
 	time0 = time.time()
 
 	# Flag which allows to recalculate bispectra, even if it is already on disk
-	recalc = False
+	recalc = True
 
 	B101_dict = {}
 	B101_dict['k1'] = []
@@ -416,13 +453,14 @@ def main():
 	B101_dict['be'] = bevo(para.redshift)
 	B101_dict['B101'] = []
 
-	theta_resolution = 36
+	theta_resolution = 20
 	# Divide the theta array according to the number of core available
 	index_low = int(rank*theta_resolution/size)
 	index_high = int((rank+1)*theta_resolution/size)
 	# Loop over k1 and k2
 	#for (k1, k2) in [(0.002,0.002), (0.005,0.005), (0.01,0.01), (0.02,0.02), (0.03,0.03)]:
-	for (k1, k2) in [(0.01,0.01)]:
+	for (k1, k2) in [(0.01*para.cosmo.h,0.01*para.cosmo.h)]: # multiply by h to to get 0.01Mpc^{-1}
+		#for (k1, k2) in [(0.1,0.1)]: # multiply by h to to get 0.01Mpc^{-1}
 		theta_12 = np.linspace(0.002, np.pi-0.002, theta_resolution)
 		# Calculate B(k1,k2,\theta_12) if needed
 		filename = r"%s/theta_12_%0.4f_%0.4f_%d.dat" % (para.outpath, k1, k2, theta_resolution)
@@ -453,9 +491,9 @@ def main():
 			BDm0 = pickle.load(open(r"%s/BDm0_%0.4f_%0.4f_%d.dat" % (para.outpath, k1, k2, theta_resolution), "rb" ))
 			BDm1 = pickle.load(open(r"%s/BDm1_%0.4f_%0.4f_%d.dat" % (para.outpath, k1, k2, theta_resolution), "rb" ))
 
-
 		if rank == 0:
 			plot_bi(theta_12, Bmono_N, BDm0, BDm1)
+			sys.exit()
 
 			# Convert to triangular spherical harmonics basis 
 			# Integral over \theta_12
@@ -467,6 +505,7 @@ def main():
 			B101_dict['k1'].append(k1)
 			B101_dict['k2'].append(k2)
 			B101_dict['B101'].append(B101)
+		comm.Barrier()
 
 	B101_dict['k1'] = np.array(B101_dict['k1'])
 	B101_dict['k2'] = np.array(B101_dict['k2'])
